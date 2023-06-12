4abb9e2797d1707a04477b0f63230648d6291956
Pascal Karisa <sirpkkarisa001@gmail.com>
2023-06-12 17:26:46 +0300

    Updated sales creation and filtering

---
+++ src/app/staff/product-sales/add-product-sale/add-product-sale.component.ts
@@ -0,0 +1,108 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { SnackbarService } from 'src/app/shared/snackbar.service';
+import { ProductSaleService } from '../service/product-sale.service';
+import { ProductSalesManagementComponent } from '../product-sales-management/product-sales-management.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-add-product-sale',
+  templateUrl: './add-product-sale.component.html',
+  styleUrls: ['./add-product-sale.component.sass']
+})
+export class AddProductSaleComponent implements OnInit {
+  productSalesAssignment: FormGroup
+  subscription!: Subscription;
+  
+  isLoading: boolean = false
+  pLoading: boolean = false
+
+  routes: any
+  customers: any
+  salesPersons: any
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: SnackbarService,
+    private salesservice: ProductSaleService,
+    public dialogRef: MatDialogRef<ProductSalesManagementComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
+
+  ngOnInit(): void {
+    this.productSalesAssignment = this.fb.group({
+      customerFk: ['', [Validators.required]],
+      productFk: ['', [Validators.required]],
+      quantity: ['', [Validators.required]],
+      routeFk: ['', [Validators.required]],
+      salesPersonFk: [''],
+    });
+
+    this.getRoutes();
+    this.getCustomers();
+    this.getSalesPersons();
+  }
+  getRoutes() {
+    this.pLoading = true
+    this.subscription = this.salesservice.fetchRoutes().subscribe((res) => {
+      if (res.entity.length > 0) {
+        this.pLoading = false
+        this.routes = res.entity;
+      } else {
+        this.pLoading = false
+        this.routes = [];
+      }
+    });
+  }
+  getCustomers() {
+    this.pLoading = true
+    this.subscription = this.salesservice.fetchCustomers().subscribe((res) => {
+      if (res.entity.length > 0) {
+        this.pLoading = false
+        this.customers = res.entity;
+
+      } else {
+        this.pLoading = false
+        this.customers = [];
+      }
+    });
+  }
+
+  getSalesPersons() {
+    this.pLoading = true
+    this.subscription = this.salesservice.fetchSalesPersons().subscribe((res) => {
+      if (res.userData.length > 0) {
+        this.pLoading = false
+        this.salesPersons = res.userData;        
+
+      } else {
+        this.pLoading = false
+        this.salesPersons = [];
+      }
+    });
+  }
+
+  onSubmit(){
+    this.isLoading = true
+
+    this.subscription = this.salesservice.addSale(this.productSalesAssignment.value)
+      .subscribe((res) => {
+        this.snackBar.showNotification('snackbar-success', 'Successful!');
+        this.isLoading = false;
+        this.productSalesAssignment.reset();
+        this.dialogRef.close();
+      },
+        (err) => {
+          console.log(err)
+          this.isLoading = false;
+          this.snackBar.showNotification('snackbar-danger', err);
+          this.dialogRef.close();
+        })
+  }
+  
+  onCancel() {
+    this.dialogRef.close()
+  }
+
+}
--- src/app/staff/product-sales/product-sales-management/product-sales-management.component.ts
+++ src/app/staff/product-sales/product-sales-management/product-sales-management.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ProductSaleService } from '../service/product-sale.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatMenuTrigger } from '@angular/material/menu';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { AddProductSaleComponent } from '../add-product-sale/add-product-sale.component';
 
 @Component({
   selector: 'app-product-sales-management',
@@ -6,10 +14,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-sales-management.component.sass']
 })
 export class ProductSalesManagementComponent implements OnInit {
+  filterform: FormGroup
+  selected = "";
 
-  constructor() { }
+  dataSource!: MatTableDataSource<any>;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild("filter", { static: true }) filter: ElementRef;
+  @ViewChild(MatMenuTrigger)
+  contextMenu: MatMenuTrigger;
+  contextMenuPosition = { x: "0px", y: "0px" };
+
+
+  displayedColumns: string[] = [
+    'id',
+    "salesPersonName",
+    "customerName",
+    "productName",
+    "amount",
+    "quantity",
+    "paymentStatus",
+    'action',
+  ];
+  data: any;
+  isdata: boolean = false;
+  isLoading: boolean = false;
+
+  constructor(
+    private salesservice: ProductSaleService,
+    private dialog: MatDialog,
+    private fb:FormBuilder,
+    ) { }
 
   ngOnInit(): void {
+    this.filterform = this.fb.group({
+      sales_code: ['']
+    })
+    this.getData()
   }
 
+  getData() {
+    this.selected = ""
+    this.isLoading = true
+    this.isdata = false
+    this.salesservice.fetchAllSales().subscribe(res => {
+      this.data = res
+      this.isLoading = false
+
+      if (res.entity.length > 0) {
+        this.isdata = true
+       
+        // Binding with the datasource
+        this.dataSource = new MatTableDataSource(res.entity);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+
+      } else {
+        this.isLoading = false
+        this.isdata = false
+        this.dataSource = new MatTableDataSource<any>(this.data);
+      }
+  
+    })
+  }
+
+  addCall(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.data = {
+      sales: ""
+    };
+    const dialogRef = this.dialog.open(AddProductSaleComponent, dialogConfig)
+    dialogRef.afterClosed().subscribe(res=>{
+      this.getData()
+    })
+  }
+
+
+
+  getSalesBySalesCode(){
+    const code = this.filterform.value.sales_code;
+    this.salesservice.fetchSalesBySalesCode(code).subscribe(res=>{
+      
+      this.data.entity = this.data.entity.filter(d=>d.id==res.entity.id);
+      this.dataSource = new MatTableDataSource(this.data.entity);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+
+    })
+  }
 }
