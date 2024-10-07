b5cc0374a00bfcad6ff4e260e11c6550e031153d
Munjiru <81191822+Munjiru@users.noreply.github.com>
2024-10-07 09:56:54 +0300

    assign warehouse

--- src/app/accountant/inventory/warehouse/warehouse-routing.module.ts
+++ src/app/accountant/inventory/warehouse/warehouse-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainComponent } from './main/main.component';
+import { InventoriesComponent } from './inventories/inventories.component';
 
 const routes: Routes = [
+  
+  {
+    path: "",
+    redirectTo:"main",
+    pathMatch: "full"
+  },
   {
     path: 'main',
-    component: MainComponent
+    component: InventoriesComponent
   }
 ];
-
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
